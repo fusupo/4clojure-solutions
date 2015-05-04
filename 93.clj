d02290e825fd9ac1bf5c61c8e0aaa771f4ce7c67
Marc Christophe <m@xtof.net>
2015-05-04 10:00:05 -0700

    more readable but much longer shitty solution to 93

--- 93.clj
+++ 93.clj
@@ -13,50 +13,65 @@
 (= (__ '((1 2)((3 4)((((5 6)))))))
    '((1 2)(3 4)(5 6)))
 
-(defn __ [x]
-  (map
-   #(loop [foo %]
-      (if (coll? (first foo))
-        (recur (first foo))
-        foo))
-   x))
+;; (defn __ [x]
+;;   (map
+;;    #(loop [foo %]
+;;       (if (coll? (first foo))
+;;         (recur (first foo))
+;;         foo))
+;;    x))
 
-;; (defn __
-;;   ([x] (__ x '()))
-;;   ([x y]
-;;    (if (coll? (first x))
-;;      (for [c x]
-;;        (__ c y))
-;;      (println x))))
-(defn __ [x]
-  (if (coll? (first x))
-    (for [c x]
-      (__ c))
-    x))
+;; ;; (defn __
+;; ;;   ([x] (__ x '()))
+;; ;;   ([x y]
+;; ;;    (if (coll? (first x))
+;; ;;      (for [c x]
+;; ;;        (__ c y))
+;; ;;      (println x))))
+;; (defn __ [x]
+;;   (if (coll? (first x))
+;;     (for [c x]
+;;       (__ c))
+;;     x))
 
-(defn __ [x]
-  (if (coll? (first x))
-    (loop [c x
-           d '()]
-      (if (empty? c) d
-          (recur (rest c) (concat d (__ (first c))))))
-    x))
+;; (defn __ [x]
+;;   (if (coll? (first x))
+;;     (loop [c x
+;;            d '()]
+;;       (if (empty? c) d
+;;           (recur (rest c) (concat d (__ (first c))))))
+;;     x))
 
 
 
 (defn __ [x]
   (loop [c false
          r x]
-    (if (= c true) r
-        (let [rr (reduce #(if (coll? (first %2))
-                            (loop [z %2 y %]
-                              (if (empty? z)
-                                y
-                                (recur (rest z) (concat y (list (first z))))))
-                            (concat % (list %2))) '() r)
-              mr (apply = (map #(coll? (first %)) rr))]
-          (recur mr rr)))))
+    (if c
+      r
+      (let [rr (reduce #(if (coll? (first %2))
+                          (loop [z %2 y %]
+                            (if (empty? z)
+                              y
+                              (recur (rest z) (concat y (list (first z))))))
+                          (concat % (list %2))) '() r)
+            mr (apply = (map #(coll? (first %)) rr))]
+        (recur mr rr)))))
 
+
+(defn __ [x]
+  (letfn [(red [d] (reduce #(if (coll? (first %2))
+                              (loop [z %2 y %]
+                                (if (empty? z)
+                                  y
+                                  (recur (rest z) (concat y (list (first z))))))
+                              (concat % (list %2))) '() d))
+          (pred [d] (= false (apply = (map #(coll? (first %)) d))))]
+    (let [fred (red x)
+          fpred (pred fred)]
+      (if fpred
+        (last (take-while pred (iterate red fred)))
+        fred))))
 
 (__ '((1 2)((3 4)((((5 6)))))))
 
